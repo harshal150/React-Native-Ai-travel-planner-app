8c88c8f0382baea836bb4b26513c87c546d5dfb3
Harshal <harshhh30601@gmail.com>
2024-07-16 19:09:40 +0530

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+useFonts({
+  // "outfit":require("../assets/fonts/Outfit-Regular.ttf"),
+  'outfit-bold':require("./../assets/fonts/Outfit-Bold.ttf"),
+  'outfit-medium':require("./../assets/fonts/Outfit-Medium.ttf"),
+  'outfit':require("./../assets/fonts/Outfit-Regular.ttf")
+})
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{
+  fontSize: 30,
+  fontFamily: 'Outfit-bold',
+  fontWeight: 'bold',   
+}}>
+  lets start the app
+</Text>
+
+    </View>
+  );
+}
